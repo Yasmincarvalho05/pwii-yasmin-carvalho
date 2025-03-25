f528239605871933ea7c19e17bf7f4c273a37578
yasmin carvalho <yasmincarvalhoreis23@gmail.com>
2025-03-25 16:31:02 -0300

    aula concatenação

---
+++ Aula 02/concatenar.php
@@ -0,0 +1,13 @@
+<?php 
+echo "hello world!<br>";
+echo "Hello world!<br>";
+echo "Hello word! <br>";
+
+$nome ="yasmin";
+$sobrenome ="carvalho";
+$numero = 5;
+
+echo $nome . "lindo" . "<br>";
+echo "${nome} <h1> ${sobrenome} </h1>";
+echo "${numero} . "<p>" . $nome ." " . $sobrenome . </p>"
+?>
